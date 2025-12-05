becb1a94ce93b3c8c54fd3c25c5af059e71e10fa
Matt Nicholson <nicholson.matt94@gmail.com>
2025-12-05 12:17:54 -0500

    cleanup

--- 2025/05/solution.py
+++ 2025/05/solution.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 class Solution:
+    """Advent of Code 2025 - Day 5 Part 1 and 2 solutions"""
 
     @classmethod
     def read_input(
@@ -10,6 +11,11 @@ class Solution:
         f_path: str | Path,
     ) -> tuple[list[tuple[int, int]], list[int]]:
         """Read puzzle input.
+
+        Parameters
+        ----------
+        f_path : str | pathlib.Path
+            Path of the input file to read.
 
         Returns
         -------
@@ -20,6 +26,7 @@ class Solution:
         """
         raw_inp = Path(f_path).read_text(encoding="utf-8").split()
 
+        # Search for the end of the ID range portion of the input
         i = 1
         curr_val = raw_inp[i]
         while "-" in curr_val:
@@ -34,13 +41,32 @@ class Solution:
         return ranges, [int(x) for x in raw_inp[i:]]
 
     @classmethod
-    def part_1(cls, id_ranges: list[tuple[int, int]], ids: list[int]) -> int:
-        """Solution to Part 1."""
-        # Use a set to avoid double-counting IDs that are contained by 2 or
-        # more ranges
+    def part_1(cls, ranges: list[tuple[int, int]], ids: list[int]) -> int:
+        """Solution to Part 1.
+
+        Parameters
+        ----------
+        ranges : list[tuple[int, int]]
+            Beginning and end values of valid ID ranges.
+        ids : list[int]
+            IDs to test.
+
+        Returns
+        -------
+        int
+            Number of valid IDs.
+
+        Breakdown
+        ---------
+        We need to identify all IDs that fall into any of the given ID ranges.
+        Since we only want to count an ID once regardless of how many ID
+        ranges it is contained by, all IDs deemed valid for a given range
+        will be added to a set. Taking the length of the set yields the
+        number of valid IDs.
+        """
         fresh = set()
 
-        for (min_id, max_id) in id_ranges:
+        for (min_id, max_id) in ranges:
             for curr_id in ids:
                 if min_id <= curr_id <= max_id:
                     fresh.add(curr_id)
@@ -48,9 +74,21 @@ class Solution:
         return len(fresh)
 
     @classmethod
-    def part_2(cls, id_ranges: list[tuple[int, int]]) -> int:
+    def part_2(cls, ranges: list[tuple[int, int]]) -> int:
         """Solution to Part 2.
 
+        Parameters
+        ----------
+        ranges : list[tuple[int, int]]
+            Beginning and end values of valid ID ranges.
+
+        Returns
+        -------
+        int
+            Sum of the sizes of all ID ranges.
+
+        Breakdown
+        ---------
         The idea here is to combine as many adjacent/overlapping ID ranges as
         we can, then sum the sizes of the ranges.
 
@@ -59,9 +97,9 @@ class Solution:
         input.
         """
         merged_ranges = []
-        id_ranges.sort()
+        ranges.sort()
 
-        for (min_id, max_id) in id_ranges:
+        for (min_id, max_id) in ranges:
             if merged_ranges and min_id <= merged_ranges[-1][1] + 1:
                 # Overlapping or adjacent - extend the last range
                 merged_ranges[-1] = (
@@ -79,9 +117,9 @@ class Solution:
 
 if __name__ == "__main__":
     input_fname = "input.txt"
-    id_ranges, ids = Solution.read_input(input_fname)
+    id_ranges, id_list = Solution.read_input(input_fname)
 
-    soln_1 = Solution.part_1(id_ranges, ids)
+    soln_1 = Solution.part_1(id_ranges, id_list)
     print(f"Solution to part 1: {soln_1}")
 
     soln_2 = Solution.part_2(id_ranges)
