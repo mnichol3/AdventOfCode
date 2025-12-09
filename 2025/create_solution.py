357d2bd7722cbdf8694c904fe7a371bcae59888b
Matt Nicholson <nicholson.matt94@gmail.com>
2025-12-09 13:36:10 -0500

    make it more better

--- 2025/create_solution.py
+++ 2025/create_solution.py
@@ -2,8 +2,7 @@ import argparse
 from pathlib import Path
 
 
-TEMPLATE = '''
-"""Advent of Code 2025 - Day {day_number} Parts 1 and 2."""
+TEMPLATE = '''"""Advent of Code 2025 - Day {day_number} Parts 1 and 2."""
 from pathlib import Path
 
 
@@ -38,10 +37,6 @@ class Solution:
         -------
         int
             Puzzle solution.
-
-        Breakdown
-        ---------
-        TODO
         """
         return 0
 
@@ -58,10 +53,6 @@ class Solution:
         -------
         int
             Puzzle solution.
-
-        Breakdown
-        ---------
-        TODO
         """
         return 0
 
@@ -71,8 +62,10 @@ if __name__ == "__main__":
     input = Solution.read_input(input_fname)
 
     soln_1 = Solution.part_1(input)
+    print(f"Solution to part 1: {{soln_1}}")
 
     soln_2 = Solution.part_2(input)
+    print(f"Solution to part 2: {{soln_2}}")
 
 '''
 
@@ -93,8 +86,11 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def create_solution_file(root_dir: Path, day_num: str) -> Path:
-    """Create the solution file."""
+if __name__ == "__main__":
+    root_dir = Path(".")
+    args = parse_args()
+    day_num = args.day_num
+
     day_dir = root_dir / day_num
     day_dir.mkdir(exist_ok=True, parents=True)
 
@@ -102,13 +98,3 @@ def create_solution_file(root_dir: Path, day_num: str) -> Path:
 
     with open(f_path, "w", encoding="utf-8") as f:
         f.write(TEMPLATE.format(day_number=day_num))
-
-    return f_path
-
-
-if __name__ == "__main__":
-    root_dir = Path(".")
-    args = parse_args()
-
-    create_solution_file(root_dir, args.day_num)
-
